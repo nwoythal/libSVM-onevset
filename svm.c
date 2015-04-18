531f670c79f9e40341f3ba3c9fc9ee00f7135b5f
John McCrummen <jmccrummen17@gmail.com>
2015-04-18 15:05:50 -0600

    Update svm.c (Read description)
    This was able to 'make' successfully (warnings but no errors) after adding one additional line to svm.h which acknowledged sv_indices. I will later attempt to get rid of as many of the warnings as possible as well as remove the "UPDATE" flags so that tboult's new svm.c has minimal diffs from libSVM's 3.20 svm.cpp of course with the exception of his additional functions

--- svm.c
+++ svm.c
@@ -64,6 +64,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <float.h>
 #include <string.h>
 #include <stdarg.h>
+//v UPDATE v//
+#include <limits.h>
+#include <locale.h>
+//^ UPDATE ^//
 #ifdef USELIBMR
 #include "MetaRecognition.h"
 #endif
@@ -640,9 +644,13 @@ void Solver::Solve(int l, const QMatrix& Q, const double *p_, const schar *y_,
 	// optimization step
 
 	int iter = 0;
+	//v UPDATE v//
+	int max_iter = max(10000000, l>INT_MAX/100 ? INT_MAX : 100*l);
+	//^ UPDATE ^//
 	int counter = min(l,1000)+1;
 
-	while(1)
+	//v UPDATE v//
+	while(iter < max_iter)
 	{
 		// show progress and do shrinking
 
@@ -809,6 +817,20 @@ void Solver::Solve(int l, const QMatrix& Q, const double *p_, const schar *y_,
 		}
 	}
 
+	//v UPDATE v//
+	if(iter >= max_iter)
+	{
+		if(active_size < l)
+		{
+			// reconstruct the whole gradient to calculate objective value
+			reconstruct_gradient();
+			active_size = l;
+			info("*");
+		}
+		fprintf(stderr,"\nWARNING: reaching max number of iterations\n");
+	}
+	//^ UPDATE ^//
+	
 	// calculate rho
 
 	si->rho = calculate_rho();
@@ -1769,7 +1791,7 @@ struct svm_model* init_svm_model(const struct svm_parameter * param){
   model->wblscale = NULL;
   model->openset_dim = 0;
   model->param.rejectedID = -9999; /* id for rejected classes (-99999 is the default) */ 
-  model->free_sv = 0;	// XXX
+  model->free_sv = 0; // XXX
   return model;
 }
 
@@ -2182,7 +2204,25 @@ static void svm_group_classes(const svm_problem *prob, int *nr_class_ret, int **
 			++nr_class;
 		}
 	}
-
+	//v UPDATE v//
+	
+	// Labels are ordered by their first occurrence in the training set. 
+	// However, for two-class sets with -1/+1 labels and -1 appears first, 
+	// we swap labels to ensure that internally the binary SVM has positive data corresponding to the +1 instances.
+	//
+	if (nr_class == 2 && label[0] == -1 && label[1] == 1)
+	{
+		swap(label[0],label[1]);
+		swap(count[0],count[1]);
+		for(i=0;i<l;i++)
+		{
+			if(data_label[i] == 0)
+				data_label[i] = 1;
+			else
+				data_label[i] = 0;
+		}
+	}
+	//^ UPDATE ^//	
 	int *start = Malloc(int,nr_class);
 	start[0] = 0;
 	for(i=1;i<nr_class;i++)
@@ -2232,19 +2272,25 @@ svm_model *svm_train_oneclass_or_regression(svm_model* model, const svm_problem 
 		model->l = nSV;
 		model->SV = Malloc(svm_node *,nSV);
 		model->sv_coef[0] = Malloc(double,nSV);
+		//v UPDATE v (original was just in general 'svm_train')//
+		model->sv_indices = Malloc(int,nSV);
+		//^ UPDATE ^//
 		int j = 0;
 		for(i=0;i<prob->l;i++)
 			if(fabs(f.alpha[i]) > 0)
 			{
 				model->SV[j] = prob->x[i];
 				model->sv_coef[0][j] = f.alpha[i];
+				//v UPDATE v (original was in 'svm_train')//
+				model->sv_indices[j] = i+1;
+				//^ UPDATE ^//
 				++j;
 			}		
 
 		free(f.alpha);
                 return model;
 }
-
+// UPDATE (not sure if sv_indices needs to be included in these other models //
 svm_model *svm_train_binary_pairs(svm_model* model, const svm_problem *prob, const svm_parameter *param){
 		// classification
 		int l = prob->l;
@@ -2256,6 +2302,10 @@ svm_model *svm_train_binary_pairs(svm_model* model, const svm_problem *prob, con
 
 		// group training data of the same class
 		svm_group_classes(prob,&nr_class,&label,&start,&count,perm);		
+		//v UPDATE v//
+		if(nr_class == 1) 
+			info("WARNING: training data in only one class. See README for details.\n");
+		//^ UPDATE ^//
 		svm_node **x = Malloc(svm_node *,l);
 		int i;
 		for(i=0;i<l;i++)
@@ -2377,10 +2427,19 @@ svm_model *svm_train_binary_pairs(svm_model* model, const svm_problem *prob, con
 
 		model->l = total_sv;
 		model->SV = Malloc(svm_node *,total_sv);
+		//v UPDATE v//
+		model->sv_indices = Malloc(int,total_sv);
+		//^ UPDATE ^//
 		p = 0;
 		for(i=0;i<l;i++)
-			if(nonzero[i]) model->SV[p++] = x[i];
-
+			if(nonzero[i])
+			{
+				//v UPDATE v//
+				//model->SV[p++] = x[i];
+				model->SV[p] = x[i];
+				model->sv_indices[p++] = perm[i] + 1;
+				//^ UPDATE ^//
+			}
 		int *nz_start = Malloc(int,nr_class);
 		nz_start[0] = 0;
 		for(i=1;i<nr_class;i++)
@@ -2431,7 +2490,7 @@ svm_model *svm_train_binary_pairs(svm_model* model, const svm_problem *prob, con
 		free(nz_start);
                 return model;
 }
-
+// UPDATE (changes may/may not need to be made to the svm_model below) //
 svm_model *svm_train_onevset_oneclass(svm_model* model, const svm_problem *prob, const svm_parameter *param){
 
   int nr_class,i;
@@ -2622,7 +2681,7 @@ svm_model *svm_train_onevset_oneclass(svm_model* model, const svm_problem *prob,
 
 
 //...........
-
+// UPDATE (svm_model here may need changes as well) //
 svm_model *svm_train_wsvm_onevset_binary(svm_model* model, const svm_problem *prob, const svm_parameter *param){
 
 #ifdef USEPIESVM
@@ -2866,7 +2925,7 @@ svm_model *svm_train_wsvm_onevset_binary(svm_model* model, const svm_problem *pr
 }
 
 
-
+// UPDATE (again, possible changes) //
 svm_model *svm_train_onevset_binary(svm_model* model, const svm_problem *prob, const svm_parameter *param){
 
   int nr_class,i;
@@ -3073,7 +3132,7 @@ svm_model *svm_train_onevset_binary(svm_model* model, const svm_problem *prob, c
   return model;
 }
  
-
+// UPDATE (another one of tboults models not found in original) //
 svm_model *svm_train_onevset_pairs(svm_model* model, const svm_problem *prob, const svm_parameter *param){
 
   int nr_class;
@@ -3115,7 +3174,7 @@ svm_model *svm_train_onevset_pairs(svm_model* model, const svm_problem *prob, co
   
   return model;
 }
-
+// UPDATE (another that may need changes) //
 svm_model *svm_train_wsvm_binary_pairs(svm_model* model, const svm_problem *prob, const svm_parameter *param){
 #ifdef USEPIESVM
 		// classification
@@ -3424,7 +3483,7 @@ svm_model *svm_train_wsvm_binary_pairs(svm_model* model, const svm_problem *prob
 #endif
   return model;
 }
-
+// UPDATE (doubt these need changes but here's a bookmark to them anyway) //
 svm_model *svm_train_wsvm_pairs(svm_model* model, const svm_problem *prob, const svm_parameter *param){
  
   model->param.svm_type = PAIR_PIESVM;
@@ -3445,6 +3504,7 @@ svm_model *svm_train_wsvm_one_vs_rest(svm_model* model, const svm_problem *prob,
 //
 // Interface functions
 //
+// UPDATE (more functions not represented in original svm) //
 svm_model *svm_train(const svm_problem *prob, const svm_parameter *param)
 {
 	svm_model *model = Malloc(svm_model,1);
@@ -3505,11 +3565,22 @@ svm_model *svm_train(const svm_problem *prob, const svm_parameter *param)
 void svm_cross_validation(const svm_problem *prob, const svm_parameter *param, int nr_fold, double *target)
 {
 	int i;
-	int *fold_start = Malloc(int,nr_fold+1);
+	//v UPDATE v//
+	//int *fold_start = Malloc(int,nr_fold+1);
+	int *fold_start;
+	//^ UPDATE ^//
 	int l = prob->l;
 	int *perm = Malloc(int,l);
 	int nr_class;
-
+	//v UPDATE v//
+	if (nr_fold > l)
+	{
+		nr_fold = l;
+		fprintf(stderr,"WARNING: # folds > # data. Will use # folds = # data instead (i.e., leave-one-out cross validation)\n");
+	}
+	fold_start = Malloc(int,nr_fold+1);
+	//^ UPDATE ^//
+	
 	// stratified cv may not give leave-one-out rate
 	// Each class to l folds -> some folds may have zero elements
 	if((param->svm_type == C_SVC ||
@@ -3640,6 +3711,19 @@ void svm_get_labels(const svm_model *model, int* label)
 		for(int i=0;i<model->nr_class;i++)
 			label[i] = model->label[i];
 }
+//v UPDATE (two functions added) v//
+void svm_get_sv_indices(const svm_model *model, int* indices)
+{
+	if (model->sv_indices != NULL)
+		for(int i=0;i<model->l;i++)
+			indices[i] = model->sv_indices[i];
+}
+
+int svm_get_nr_sv(const svm_model *model)
+{
+	return model->l;
+}
+//^ UPDATE ^//
 
 double svm_get_svr_probability(const svm_model *model)
 {
@@ -4214,6 +4298,11 @@ int svm_save_model(const char *model_file_name, const svm_model *model)
 	FILE *fp = fopen(model_file_name,"w");
 	if(fp==NULL) return -1;
 
+	//v UPDATE v//
+	char *old_locale = strdup(setlocale(LC_ALL, NULL));
+	setlocale(LC_ALL, "C");
+	//^ UPDATE ^//
+	
 	const svm_parameter& param = model->param;
 
     //printf("%g %g\n", param.min_thresh, param.max_thresh);
@@ -4358,6 +4447,10 @@ int svm_save_model(const char *model_file_name, const svm_model *model)
 			}
 		fprintf(fp, "\n");
 	}
+	//v UPDATE v//
+	setlocale(LC_ALL, old_locale);
+	free(old_locale);
+	//^ UPDATE ^//
 	if (ferror(fp) != 0 || fclose(fp) != 0) return -1;
 	else return 0;
 }
@@ -4387,12 +4480,131 @@ void svm_badload_cleanup(svm_model *model){
   }
 }
 
+//v UPDATE (this whole large function was added) v//
+// Its do-while block avoids the ambiguity when
+// if (...)
+//    FSCANF();
+// is used
+//
+#define FSCANF(_stream, _format, _var) do{ if (fscanf(_stream, _format, _var) != 1) return false; }while(0)
+bool read_model_header(FILE *fp, svm_model* model)
+{
+	svm_parameter& param = model->param;
+	char cmd[81];
+	while(1)
+	{
+		FSCANF(fp,"%80s",cmd);
 
+		if(strcmp(cmd,"svm_type")==0)
+		{
+			FSCANF(fp,"%80s",cmd);
+			int i;
+			for(i=0;svm_type_table[i];i++)
+			{
+				if(strcmp(svm_type_table[i],cmd)==0)
+				{
+					param.svm_type=i;
+					break;
+				}
+			}
+			if(svm_type_table[i] == NULL)
+			{
+				fprintf(stderr,"unknown svm type.\n");
+				return false;
+			}
+		}
+		else if(strcmp(cmd,"kernel_type")==0)
+		{		
+			FSCANF(fp,"%80s",cmd);
+			int i;
+			for(i=0;kernel_type_table[i];i++)
+			{
+				if(strcmp(kernel_type_table[i],cmd)==0)
+				{
+					param.kernel_type=i;
+					break;
+				}
+			}
+			if(kernel_type_table[i] == NULL)
+			{
+				fprintf(stderr,"unknown kernel function.\n");	
+				return false;
+			}
+		}
+		else if(strcmp(cmd,"degree")==0)
+			FSCANF(fp,"%d",&param.degree);
+		else if(strcmp(cmd,"gamma")==0)
+			FSCANF(fp,"%lf",&param.gamma);
+		else if(strcmp(cmd,"coef0")==0)
+			FSCANF(fp,"%lf",&param.coef0);
+		else if(strcmp(cmd,"nr_class")==0)
+			FSCANF(fp,"%d",&model->nr_class);
+		else if(strcmp(cmd,"total_sv")==0)
+			FSCANF(fp,"%d",&model->l);
+		else if(strcmp(cmd,"rho")==0)
+		{
+			int n = model->nr_class * (model->nr_class-1)/2;
+			model->rho = Malloc(double,n);
+			for(int i=0;i<n;i++)
+				FSCANF(fp,"%lf",&model->rho[i]);
+		}
+		else if(strcmp(cmd,"label")==0)
+		{
+			int n = model->nr_class;
+			model->label = Malloc(int,n);
+			for(int i=0;i<n;i++)
+				FSCANF(fp,"%d",&model->label[i]);
+		}
+		else if(strcmp(cmd,"probA")==0)
+		{
+			int n = model->nr_class * (model->nr_class-1)/2;
+			model->probA = Malloc(double,n);
+			for(int i=0;i<n;i++)
+				FSCANF(fp,"%lf",&model->probA[i]);
+		}
+		else if(strcmp(cmd,"probB")==0)
+		{
+			int n = model->nr_class * (model->nr_class-1)/2;
+			model->probB = Malloc(double,n);
+			for(int i=0;i<n;i++)
+				FSCANF(fp,"%lf",&model->probB[i]);
+		}
+		else if(strcmp(cmd,"nr_sv")==0)
+		{
+			int n = model->nr_class;
+			model->nSV = Malloc(int,n);
+			for(int i=0;i<n;i++)
+				FSCANF(fp,"%d",&model->nSV[i]);
+		}
+		else if(strcmp(cmd,"SV")==0)
+		{
+			while(1)
+			{
+				int c = getc(fp);
+				if(c==EOF || c=='\n') break;
+			}
+			break;
+		}
+		else
+		{
+			fprintf(stderr,"unknown text in model file: [%s]\n",cmd);
+			return false;
+		}
+	}
+
+	return true;
+
+}
+//^ UPDATE ^ (end of added function) //
 svm_model *svm_load_model(const char *model_file_name)
 {
 	FILE *fp = fopen(model_file_name,"rb");
 	if(fp==NULL) return NULL;
 	
+	//v UPDATE v//
+	char *old_locale = strdup(setlocale(LC_ALL, NULL));
+	setlocale(LC_ALL, "C");
+	//^ UPDATE ^//
 	// read parameters
 
 	svm_model *model = Malloc(svm_model,1);
@@ -4807,6 +5019,10 @@ svm_model *svm_load_model(const char *model_file_name)
 	}
 	free(line);
 
+	//v UPDATE v//
+	setlocale(LC_ALL, old_locale);
+	free(old_locale);
+	//^ UPDATE ^//
 	if (ferror(fp) != 0 || fclose(fp) != 0)
 		return NULL;
 
@@ -4853,6 +5069,10 @@ void svm_free_model_content(svm_model* model_ptr)
 	free(model_ptr->probB);
 	model_ptr->probB= NULL;
 
+	//v UPDATE v//
+	free(model_ptr->sv_indices);
+	model_ptr->sv_indices = NULL;
+	//^ UPDATE ^//
 	free(model_ptr->nSV);
 	model_ptr->nSV = NULL;
 
